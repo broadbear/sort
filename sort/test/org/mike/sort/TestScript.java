b747c06b27dabb2f8b185863bdd496ac1199dfae
Michael Broadbear <broadbear@gmail.com>
2013-11-16 10:03:12 -0600

    - refactored test script

--- sort/test/org/mike/sort/TestScript.java
+++ sort/test/org/mike/sort/TestScript.java
@@ -1,5 +1,6 @@
 package org.mike.sort;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,6 +8,7 @@ public class TestScript {
 
 	public static void main(String[] args) {
 		int n = 5000000;
+		int iterations = 20;
 		SortStrategy<Integer> psrs = new SortStrategy<Integer>() {
 			public List<Integer> sort(List<Integer> unsorted) {
 				List<Integer> sorted = PCollections.sort(unsorted);
@@ -20,19 +22,24 @@ public class TestScript {
 				return unsorted;
 			}
 		};
-		
-		System.out.println("collections");
-		for (int i = 0; i < 20; i++) {
-			long time = testSort(n, std);
-			System.out.println("sort["+i+"] time["+time+"]");
-		}
-		System.out.println("pcollections");
-		for (int i = 0; i < 20; i++) {
-			long time = testSort(n, psrs);
-			System.out.println("sort["+i+"] time["+time+"]");
-		}
+
+		testSorts("collections", n, std, iterations);
+		testSorts("pcollections", n, psrs, iterations);
 	}
 
+	static <T> long testSorts(String id, int n, SortStrategy<Integer> s, int iterations) {
+		System.out.println(id);
+		List<Long> times = new ArrayList<Long>();
+		for (int i = 0; i < iterations; i++) {
+			long time = testSort(n, s);
+			times.add(time);
+			System.out.println("sort["+i+"] time["+time+"]");
+		}
+		long mean = calculateMean(times);		
+		System.out.println("mean["+mean+"]");
+		return mean;
+	}
+	
 	static <T> long testSort(int n, SortStrategy<Integer> s) {
 		List<Integer> list = Harness.createList(n);
 		long start = System.currentTimeMillis();
@@ -41,6 +48,15 @@ public class TestScript {
 		long time = end - start;
 		Harness.verify(sorted);
 		return time;
+	}
+
+	public static long calculateMean(List<Long> set) {
+		long numerator = 0;
+		for (Long i: set) {
+			numerator += i;
+		}
+		long result = (long) Math.floor(numerator / set.size());
+		return result;
 	}
 
 	static abstract class SortStrategy<T> {
